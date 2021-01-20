ed790e29dda48cb26ed4f39c21e8cf7c30221526
Little ant <37583768+mayupsc@users.noreply.github.com>
2021-01-20 21:15:15 +0800

    Update phyloTree_DECIPHER.R

--- script/phyloTree_DECIPHER.R
+++ script/phyloTree_DECIPHER.R
@@ -1,8 +1,10 @@
-library(DECIPHER)
-nucleotide_seq_msa_file <- 'database/virus_msa.fasta'
+suppressPackageStartupMessages(library(DECIPHER))
+
+argv <- commandArgs(T)
+nucleotide_seq_msa_file <- argv[1] 
+constree_file <- argv[2]
 
 nucleotide_sequences <- readDNAStringSet(nucleotide_seq_msa_file, format="fasta")
 d <- DistanceMatrix(nucleotide_sequences,correction = "Jukes-Cantor",verbose=FALSE)
 dend <- IdClusters(d,method="ML",type = "dendrogram",myXStringSet=nucleotide_sequences)
-WriteDendrogram(dend,file="database/constree")
-
+WriteDendrogram(dend,file=constree_file)
