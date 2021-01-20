3f4ea23c4cce052053d7f1007e11a47916144acd
Little ant <37583768+mayupsc@users.noreply.github.com>
2021-01-20 16:06:36 +0800

    Update rundocker_daemon.sh

--- rundocker_daemon.sh
+++ rundocker_daemon.sh
@@ -1,4 +1,4 @@
 docker stop shiny2 
 docker rm shiny2 
-docker run --name shiny2 -d -p 3838:3838 -v /Users/mayu/Documents/huangtan/vORFfinder2.0/:/srv/shiny-server/ yumapsc/vorffinder2.1
+docker run --name shiny2 -d -p 8383:8383 -v $PWD:/srv/shiny-server/ yumapsc/vorffinder2.1
 sudo docker exec -ti shiny2 /bin/bash
