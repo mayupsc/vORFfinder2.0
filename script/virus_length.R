2bde2350ab736b3c9c8c3e904438950f1da75587
Little ant <37583768+mayupsc@users.noreply.github.com>
2021-01-20 21:14:12 +0800

    Update virus_length.R

--- script/virus_length.R
+++ script/virus_length.R
@@ -1,9 +1,13 @@
 library(phylotools)
 
-virus_fasta_file <- 'database/virus_db.fasta'
+argv <- commandArgs(T)
+
+virus_fasta_file <- argv[1]
+virus_length_file <- argv[2] 
+
 virus_fasta <- read.fasta(virus_fasta_file)
 
 virus_fasta$len <- apply(virus_fasta,1,function(x){nchar(x[2])})
 colnames(virus_fasta) <- c('virus','seq','length')
 
-write.table(virus_fasta,'database/virus_length.txt',sep = "\t",quote = F,row.names = F,col.names = T)
+write.table(virus_fasta,virus_length_file,sep = "\t",quote = F,row.names = F,col.names = T)
