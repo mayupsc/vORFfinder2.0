167af4120f1b44477b4447393f0190ff073645ee
Little ant <37583768+mayupsc@users.noreply.github.com>
2021-01-20 21:14:46 +0800

    Update calc.sh

--- script/calc.sh
+++ script/calc.sh
@@ -1,58 +1,54 @@
-##-- 0. init
-rm -r database blastp ORF
-mkdir -p  blastp database/par  ORF
-chmod 777 blastp database ORF
-
 ##-- 1. receive user input file and parameters
-file=$1
-orf_len=$2
-start_codon=$3
-genetic_code=$4
-ignore_nest=$5
+taskID=$1
+file=$2
+orf_len=$3
+start_codon=$4
+genetic_code=$5
+ignore_nest=$6
 
-filename=`basename $file|sed 's/.zip//g'`
+mkdir -p  $taskID/blastp $taskID/database $taskID/ORF $taskID/log
+chmod 777 $taskID/blastp $taskID/database $taskID/ORF $taskID/log
 
+
+printf "your taskID is : "$taskID"\n"
 printf "ORFfinder parameters are setting to:\n"
 printf "ORF length : "$orf_len"\n"
 printf "start codon : "$start_codon"\n"
 printf "genetic code : "$genetic_code"\n"
 printf "ignore_nest : "$ignore_nest"\n"
-printf "your file is "$file"\nfiles including:\n"
+printf "your file including:\n"
 
-unzip $file -d database/virus_seq
-mv database/virus_seq/*/* database/virus_seq
+unzip $file -d $taskID/database/virus_seq
+mv $taskID/database/virus_seq/*/* $taskID/database/virus_seq
 
-##--  2. some prepare work 
+##--  2. some prepare work
 
 #format fasta files included in zip file
-for id in $(ls database/virus_seq/*fasta); do ID=`echo $id|sed 's/.*\\///g'|sed 's/.fasta//g'`;sed -i "s/>.*/>$ID/g" $id; done
+for id in $(ls $taskID/database/virus_seq/*fasta); do ID=`echo $id|sed 's/.*\\///g'|sed 's/.fasta//g'`;sed -i "s/>.*/>$ID/g" $id; done
 
-cat database/virus_seq/*fasta > database/virus_db.fasta
-Rscript /srv/shiny-server/script/virus_length.R
+cat $taskID/database/virus_seq/*fasta > $taskID/database/virus_db.fasta
+Rscript script/virus_length.R $taskID/database/virus_db.fasta $taskID/database/virus_length.txt
 
-/opt/conda/bin/mafft database/virus_db.fasta > database/virus_msa.fasta
-
+/opt/conda/bin/mafft --quiet $taskID/database/virus_db.fasta > $taskID/database/virus_msa.fasta
 
 ##-- 3. construct phylogenetic tree : DECIPHER
-Rscript /srv/shiny-server/script/phyloTree_DECIPHER.R
-sed -i 's/\"//g' database/constree
+Rscript script/phyloTree_DECIPHER.R $taskID/database/virus_msa.fasta $taskID/database/constree> $taskID/log/step3_phyloTree_DECIPHER.log 
+sed -i 's/\"//g' $taskID/database/constree
 
 ##-- 4. make ORF database
 
-for id in $(ls database/virus_seq/*fasta); do { sample=`echo ${id}|sed 's/.*\///g'|sed 's/.fasta//g'`; /srv/shiny-server/software/ORFfinder -in $id -ml $orf_len -s $start_codon -g $genetic_code -n $ignore_nest -out ORF/${sample}.orfs; }& done ;wait
-sed -i 's/lcl|//g' ORF/*.orfs
-sed -i 's/ unnamed protein product.*//g' ORF/*.orfs
+for id in $(ls $taskID/database/virus_seq/*fasta); do { sample=`echo ${id}|sed 's/.*\///g'|sed 's/.fasta//g'`; software/ORFfinder -in $id -ml $orf_len -s $start_codon -g $genetic_code -n $ignore_nest -out $taskID/ORF/${sample}.orfs >> $taskID/log/step4_make_orf_db.log; }& done ;wait
+sed -i 's/lcl|//g' $taskID/ORF/*.orfs
+sed -i 's/ unnamed protein product.*//g' $taskID/ORF/*.orfs
 
-cat ORF/*.orfs > database/orf_db.fasta
-/opt/conda/bin/makeblastdb -in database/orf_db.fasta -parse_seqids -dbtype prot
-
-##-- 5. /opt/conda/bin/blastp
-for id in $(ls ORF/*orfs); do { sample=`echo ${id}|sed 's/.*\///g'|sed 's/.orfs//g'`; /opt/conda/bin/blastp -query $id -db database/orf_db.fasta -outfmt 10 -evalue 0.05 > blastp/${sample}.blast; }& done ; wait
-for id in $(ls blastp/*blast); do { Rscript /srv/shiny-server/script/blast_process.R $id ; }& done ; wait
-for id in $(ls blastp/*fa); do { /opt/conda/bin/mafft $id > ${id%%.fa}.msa.fa; }& done; wait
-#for id in $(ls blastp/*msa.fa); do Rscript /srv/shiny-server/script/msa_html.R $id; done
-
+cat $taskID/ORF/*.orfs > $taskID/database/orf_db.fasta
+/opt/conda/bin/makeblastdb -in $taskID/database/orf_db.fasta -parse_seqids -dbtype prot >> $taskID/log/step4_make_orf_db.log
+##-- 5. blastp
+for id in $(ls $taskID/ORF/*orfs); do { sample=`echo ${id}|sed 's/.*\///g'|sed 's/.orfs//g'`; /opt/conda/bin/blastp -query $id -db $taskID/database/orf_db.fasta -outfmt 10 -evalue 0.05 > $taskID/blastp/${sample}.blast ; }& done ; wait
+for id in $(ls $taskID/blastp/*blast); do { Rscript script/blast_process.R $id $taskID >> $taskID/log/step5_blastp.log ; }& done ; wait
+for id in $(ls $taskID/blastp/*fa); do { /opt/conda/bin/mafft --quiet $id > ${id%%.fa}.msa.fa; }& done; wait
+#for id in $(ls blastp/*msa.fa); do Rscript script/msa_html.R $id>> log/step5_blastp.log ; done
 
 ## files for orf viewer
-for id in $(ls ORF/*orfs); do { Rscript /srv/shiny-server/script/orf_process.R $id; }& done
+for id in $(ls $taskID/ORF/*orfs); do { Rscript script/orf_process.R $id $taskID>> $taskID/log/step6_orfviewer.log; }& done
 
